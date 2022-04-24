56ab04c622198645b5e789cbc6af6265d912d2a7
riadmathieu <100683190+riadmathieu@users.noreply.github.com>
2022-04-24 11:03:37 +0200

    Update Log.py

--- TP4/Discord_Bot/Log.py
+++ TP4/Discord_Bot/Log.py
@@ -5,13 +5,13 @@ class Log(): # declaration de la classe
     def __init__(self): #self est le premier argument de la classe
         logging.basicConfig(filename='l.log', format='%(filename)s: %(message)s', lvl=logging.DEBUG) # on utulise DEBUG pour obtenir des information detailles. C'est utile quand on diagnostique un probleme
 
-    def ErrorMsg(self):
+    def Erreur(self):
         logging.warning("message d'alerte") # on utulise warning pour dire que quelque chose d'innatendue a eu lieu
     
     
-    def infolog(self,messages):
+    def information(self,messages):
         logging.info(str(messages)) # on utilise info pour dire que tous c'est passé comme prevu
         print(str(messages))
 
-    def SaveMsg(self,auteur,messages):
+    def Sauvegarder(self,auteur,messages):
         logging.debug(str(auteur)+": "+str(messages))
